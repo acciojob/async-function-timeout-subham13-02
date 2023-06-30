126c5a066cae4f4adf12fa51e567cb5dc0d42e85
Subham Sahu <115224258+subham13-02@users.noreply.github.com>
2023-06-30 22:46:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+// script.js
+document.getElementById('btn').addEventListener('click', showMessage);
+
+async function showMessage() {
+  const textInput = document.getElementById('text').value;
+  const delayInput = document.getElementById('delay').value;
+  const outputDiv = document.getElementById('output');
+
+  // Clear previous output
+  outputDiv.innerHTML = '';
+
+  // Validate inputs
+  if (!textInput || !delayInput) {
+    outputDiv.innerText = 'Please enter a message and a delay.';
+    return;
+  }
+
+  const delay = parseInt(delayInput);
+
+  try {
+    // Wait for the specified delay before resolving with the message
+    await delayAsync(delay);
+
+    // Display the message on the webpage
+    outputDiv.innerText = textInput;
+  } catch (error) {
+    // Handle any errors that occur during the delay
+    outputDiv.innerText = 'An error occurred.';
+    console.error(error);
+  }
+}
+
+function delayAsync(delay) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
